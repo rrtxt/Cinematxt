0b14161df25f0e70142b81f03de46be1ffd6398e
rrtxt <rizfiferdian@gmail.com>
2024-01-05 20:03:24 +0700

    Change /movies GET

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,7 +1,7 @@
 import seedData from "@/app/database/seeders/movieSeeder";
 import { NextResponse } from "next/server";
 
-export async function GET(res : NextResponse) {
-    await seedData()
-    return NextResponse.json({success : 'success'})
-}
+// export async function GET(res : NextResponse) {
+//     await seedData()
+//     return NextResponse.json({success : 'success'})
+// }
